262a2f07c81de6963935e1ab653b7906d53df307
Melanie Villela <melanienvillela@gmail.com>
2019-04-10 16:43:32 -0500

    Update
    Working on carousel...

--- js/script.js
+++ js/script.js
@@ -1,118 +1,92 @@
-import projects from './projects.js';
-console.log(projects)
+//Remove display none to divs
+const content = $(".content");
+content.removeClass("hide-init");
+
+//List of projects to display
+const projectList = [
+    {
+        name: "Lotus Fitness",
+        largeImage: "fitness-classes.png",
+        smallImage: "fitness-classes-s.png"
+    },
+    {
+        name: "Houston Eats Mexican",
+        largeImage: "houston-eats.png",
+        smallImage: "houston-eats-s.png"
+    },
+    {
+        name: "Houston Art Crawl",
+        largeImage: "houston-art-crawl.png",
+        smallImage: "houston-art-crawl-s.png"
+    },
+    {
+        name: "Dumpy House Buyers",
+        largeImage: "dumpy-house-buyers.png",
+        smallImage: "dumpy-house-buyers-s.png"
+    },
+    {
+        name: "The Adventures of Little Tikka",
+        largeImage: "little-tikka.png",
+        smallImage: "little-tikka-s.png"
+    },
+    {
+        name: "Holiday Potluck",
+        largeImage: "pot-luck.png",
+        smallImage: "pot-luck-s.png"
+    },
+    {
+        name: "Where To Eat",
+        largeImage: "restaurant-picker.png",
+        smallImage: "restaurant-picker-s.png"
+    },
+];
+
 //Header navbar functionality
 const nav = $(".nav");
 nav.click(function() {
     switch($(this).html()) {
         case "MV":       
-            $(".content").removeClass("show");        
-            $(".content").addClass("hide");
+            content.removeClass("show");        
+            content.addClass("hide");
             $("#main").removeClass("hide");
             $("#main").addClass("show");
             break;
         case "About":   
-            $(".content").removeClass("show");    
-            $(".content").addClass("hide");
+            content.removeClass("show");    
+            content.addClass("hide");
             $("#about").removeClass("hide");
             $("#about").addClass("show");
             break;
         case "Skills":  
-            $(".content").removeClass("show");        
-            $(".content").addClass("hide");
+            content.removeClass("show");        
+            content.addClass("hide");
             $("#skills").removeClass("hide");
             $("#skills").addClass("show");
             break;
         case "Projects": 
-            $(".content").removeClass("show");            
-            $(".content").addClass("hide");
+            content.removeClass("show");            
+            content.addClass("hide");
             $("#projects").removeClass("hide");
             $("#projects").addClass("show");
             break;
         case "Contact":    
-            $(".content").removeClass("show");        
-            $(".content").addClass("hide");
+            content.removeClass("show");        
+            content.addClass("hide");
             $("#contact").removeClass("hide");
             $("#contact").addClass("show");
             break;
     }
 });
 
-/*
-//Resize Font Awesome icons depending on screensize
-window.addEventListener("resize", resizeScreen);
+function imageCarousel() {
+    const projectImage = $("img");
+    const projectDescription = $("#project-descriptions");
 
-//Add "X" to modal on smaller screensize
-window.addEventListener("resize", closeModal);
-
-//Resize Screen
-function resizeScreen() {
-    const icons = document.querySelectorAll("i");
-
-    if (window.screen.width < 730) {
-        icons.forEach(icon => {
-            icon.classList.remove("fa-3x");
-            icon.classList.add("fa-2x");
-        });
-    } else {
-        icons.forEach(icon => {
-            icon.classList.remove("fa-2x");
-            icon.classList.add("fa-3x");
-        });
+    for(let i=0; i<projectList.length; i++) {
+        console.log(i)
+        projectImage.attr("src", `./img/${projectList[i].smallImage}`)
+        //let image = $(`<img src="${pojectImages[i].smallImage}" alt="${pojectImages[i].name}">`);
     }
-};
-
-//Show modal
-let projects = document.querySelectorAll(".project");
-
-projects.forEach((project) => {
-    project.addEventListener("mouseenter", function() {
-        this.children[1].classList.remove("hide");
-        this.addEventListener("mouseleave", function() {
-            this.children[1].classList.add("hide");
-        })
-    });
-});
-
-//Manually close modal
-function closeModal() {
-    const closeIcon = document.querySelectorAll(".close");
-
-    if (window.screen.width < 900) {
-        closeIcon.forEach(icon => {
-            icon.classList.remove("hide");
-            icon.addEventListener("click", function() {
-                this.parentNode.classList.add("hide");
-            })
-        })
-    }    
-};
-
-//Change letter colors & fonts in name
-const letters = document.querySelectorAll("h1");
-
-changeColors = () => {
-    const random = Math.floor((Math.random() * letters.length));
-    if (random % 2 == 0) {
-        letters[random].classList.toggle("purple");
-    } else {letters[random].classList.toggle("green")}
 }
 
-//Change style sheet
-const magic = document.getElementById("stylize");
-
-magic.addEventListener("click", function() {
-    const styleSheet = document.head.children[4];
-    
-    if (styleSheet.getAttribute("href") === "css/style-classy.css") {
-        styleSheet.setAttribute("href", "css/style-funky.css")
-    } 
-    else {console.log("Funky")      
-        styleSheet.setAttribute("href", "css/style-classy.css")
-    }
-})
-
-window.setInterval(changeColors, 300);
-
-resizeScreen();
-closeModal();
-*/
+imageCarousel();
