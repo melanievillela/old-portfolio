b19857e0fe306b253fcef9b190916744f2907a43
Melanie Villela <melanienvillela@gmail.com>
2019-04-22 15:01:19 -0500

    Update
    Made images in mobile and desktop versions

--- js/script.js
+++ js/script.js
@@ -43,3 +43,38 @@ projects.each(function() {
         })
     });
 });
+
+//Images
+const mobileImages = [
+    "img/dumpy-house-buyers-s.png",
+    "img/fitness-classes-s.png",
+    "img/houston-art-crawl-s.png",
+    "img/houston-eats-s.png",
+    "img/NASA-bday-s.png",
+    "img/little-tikka-s.png",
+    "img/invitations-s.png",
+    "img/medel-s.png"
+];
+
+const desktopImages = [
+    "img/dumpy-house-buyers.png",
+    "img/fitness-classes.png",
+    "img/houston-art-crawl.png",
+    "img/houston-eats.png",
+    "img/NASA-bday.png",
+    "img/little-tikka.png",
+    "img/invitations.png",
+    "img/medel.png"
+];
+
+const imageDIV = $("img");
+
+if (window.screen.width < 500) {
+    imageDIV.each(function(index, element) {
+        $(element).attr("src", mobileImages[index])
+    }); 
+}   else {
+    imageDIV.each(function(index, element) {
+        $(element).attr("src", desktopImages[index])
+    }); 
+}
